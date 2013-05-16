9a43f13fdc20694eca2c7bdd28451266a081b9dc
Qin Yuan <qinyuanz@cn6000.com>
2013-05-16 14:53:06 +0800

    添加HallScene

--- Published-HTML5/resources-html5.js
+++ Published-HTML5/resources-html5.js
@@ -1,14 +1,16 @@
 var ccb_resources = [
     {type:'ccbi', src:"MainScene.ccbi"},
-    {type:'image', src:"ccbResources/landing.png"},
     {type:'plist', src:"ccbResources/ccbDefaultImages.plist"},
-    {type:'image', src:"ccbResources/ccbParticleSmoke.png"},
+    {type:'image', src:"ccbResources/landing.png"},
     {type:'ccbi', src:"LandingScene.ccbi"},
     {type:'image', src:"ccbResources/ccbDefaultImages.png"},
     {type:'plist', src:"ccbResources/landing.plist"},
     {type:'image', src:"ccbResources/ccbParticleFire.png"},
     {type:'image', src:"clickme.png"},
     {type:'image', src:"ccbResources/ccbParticleStars.png"},
+    {type:'ccbi', src:"ProgressScene.ccbi"},
+    {type:'ccbi', src:"HallScene.ccbi"},
     {type:'image', src:"clickme-down.png"},
+    {type:'image', src:"ccbResources/ccbParticleSmoke.png"},
     {type:'image', src:"ccbResources/ccbParticleSnow.png"},
 ];
