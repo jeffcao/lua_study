1c9be398dfbb2b2a51010c86f8ee87e34828a49e
Jeff Cao <jeffcao@cn6000.com>
2013-06-24 18:55:57 +0800

    实现反馈界面

--- Resources/FeedbackScene.lua
+++ Resources/FeedbackScene.lua
@@ -1,36 +1,36 @@
 require "src.UIControllerPlugin"
 require "src.HallServerConnectionPlugin"
 
-FeedbackScene = class("FeedbackScene", function() return display.newScene("FeedbackScene") end)
+FeedbackScene = class("FeedbackScene", function() 
+		return display.newScene("FeedbackScene") 
+	end)
 
 function createFeedbackScene() return FeedbackScene.new() end
 
 function FeedbackScene:ctor()
 	ccb.feedback_scene = self
 	
+	self.on_ui_commit_btn_clicked = __bind(self.do_ui_commit_btn_clicked, self)
+	
 	local ccbproxy = CCBProxy:create()
  	local node = CCBReaderLoad("FeedbackScene.ccbi", ccbproxy, false, "")
  	
- 	self.input_png = "ccbResources/kuang01.png"
+ 	self.input_png = "kuang11.png"
 	self.nick_name_box = self:addEditbox(self.feed_layer, 500, 200, false, 101)
+	self.nick_name_box:setInputMode(kEditBoxInputModeAny)
 	
 	local layer = createFullMubanStyleLayer()
 	self:addChild(layer)
-	layer:setTitle("ccbResources/fan01.png")
+	layer:setTitle("biaoti09.png")
 	
 	layer:setContent(node)
 
 end
 
-function FeedbackScene:onKeypad(key)
-	if key == "backClicked" then
-		self:doToHall()
-	end
-end
-
-function FeedbackScene:doToHall()
-	--local scene = createHallScene()
-	CCDirector:sharedDirector():popScene()
+function FeedbackScene:do_ui_commit_btn_clicked(tag, sender)
+	print("[FeedbackScene:do_ui_commit_btn_clicked]")
+	
+	
 end
 
 UIControllerPlugin.bind(FeedbackScene)
