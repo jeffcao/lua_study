ecffbbca6250ec943e1c7e52aec3149753c8515c
Jeff Cao <jeffcao@cn6000.com>
2013-06-25 14:32:41 +0800

    实现商场列表

--- Resources/MarketItem.lua
+++ Resources/MarketItem.lua
@@ -1,3 +1,4 @@
+
 MarketItem = class("MarketItem", function() 
 	print("new Market item")
 	return display.newLayer("MarketItem")
@@ -10,14 +11,37 @@ function createMarketItem()
 end
 
 function MarketItem:ctor()
-	self.ccbproxy = CCBProxy:create()
- 	self.ccbproxy:retain()
- 	
- 	local node = self.ccbproxy:readCCBFromFile("MarketItem.ccbi")
-	assert(node, "failed to load hall scene")
-	self.rootNode = tolua.cast(node, "CCLayer")
+
+	ccb.matket_item_scene = self
+	self.on_ui_buy_btn_clicked = __bind(self.do_ui_buy_btn_clicked, self)
+
+	local ccbproxy = CCBProxy:create()
+	local node = CCBReaderLoad("MarketItem.ccbi", ccbproxy, false, "")
+	self:addChild(node)
+
 	self.rootNode:ignoreAnchorPointForPosition(false)
 	self.rootNode:setAnchorPoint(ccp(0,0.5))
-	self:addChild(self.rootNode)
+
 	scaleNode(self.rootNode, GlobalSetting.content_scale_factor)
 end
+
+function MarketItem:init_item(product)
+	print("[MarketItem:init_item]")
+	self.product = product
+	local name_lb = tolua.cast(self.name_lb, "CCLabelTTF")
+	name_lb:setString(product.name)
+	
+	local note_lb = tolua.cast(self.note_lb, "CCLabelTTF")
+	note_lb:setString(product.note)
+	
+	local price_lb = tolua.cast(self.price_lb, "CCLabelTTF")
+	price_lb:setString("价格 "..product.rmb.."元")
+
+	
+end
+
+function MarketItem:do_ui_buy_btn_clicked(tag, sender)
+	print("[MarketItem:do_ui_buy_btn_clicked]")
+	
+end
+
--- proj.android/assets/MarketScene.lua
+++ proj.android/assets/MarketScene.lua
@@ -1,4 +1,7 @@
 require "src.MarketSceneUPlugin"
+require "src.UIControllerPlugin"
+require "src.HallServerConnectionPlugin"
+
 MarketScene = class("MarketScene", function()
 	print("new market scene")
 	return display.newScene("MarketScene")	
@@ -11,15 +14,19 @@ function createMarketScene()
 end
 
 function MarketScene:ctor()
-	self.ccbproxy = CCBProxy:create()
-	self.ccbproxy:retain()
+	
+	local ccbproxy = CCBProxy:create()
 	
 	local layer = createFullMubanStyleLayer()
 	self.rootNode = layer
 	self:addChild(layer)
 	
 	layer:setTitle("biaoti06.png")
-	local list_view = self:createListView()
-	layer:setContent(list_view)
+	
+	self:init_product_list()
+	
 end
+
 MarketSceneUPlugin.bind(MarketScene)
+UIControllerPlugin.bind(MarketScene)
+HallServerConnectionPlugin.bind(MarketScene)
