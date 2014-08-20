db3da71e8598b4f240ac971a38fc38d6ba4460ef
qinyuanz <qinyuanz@cn6000.com>
2014-08-20 16:52:42 +0800

    修复密码找回时位数限制的bug

--- Resources/ForgetPasswordScene.lua
+++ Resources/ForgetPasswordScene.lua
@@ -27,7 +27,7 @@ function ForgetPasswordScene:ctor()
  	self.input_png = "kuang_a.png"
 	self.user_id_box = self:addEditbox(self.user_id_box_layer, 300, 50, false, 101)
 	self.user_id_box:setInputMode(kEditBoxInputModeNumeric)
-	self.user_id_box:setMaxLength(5)
+	self.user_id_box:setMaxLength(10)
 	self.user_id_box:setPlaceHolder(" 请输入帐号(如:12345)")
 	self.mail_box = self:addEditbox(self.mail_box_layer, 300, 50, false, 101)
 	self.mail_box:setPlaceHolder(" example@example.com")
