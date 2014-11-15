dca5cf859400f34fc23fe304087b2ca4920a2110
edwardzhou <edward.zhq@gmail.com>
2014-11-15 12:26:33 +0800

    refine build_native.py with extra parameter DDZ_DEBUG={1|0}

--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -3,3 +3,11 @@ APP_STL := gnustl_static
 APP_CPPFLAGS := -DCOCOS2D_DEBUG=1 -D__GXX_EXPERIMENTAL_CXX0X__ -std=c++11 -frtti -Wno-error=format-security -fsigned-char -Os $(CPPFLAGS)
 
 APP_PLATFORM := android-8
+
+
+ifeq ($(DDZ_DEBUG),1)
+  APP_CPPFLAGS += -DDDZ_DEBUG=1
+  APP_OPTIM := debug
+else
+  APP_OPTIM := release
+endif
